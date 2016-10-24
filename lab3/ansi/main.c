f58bc104562c1d55cb0d7e50ad50c3486c7ada69
KiraLow <3oonekro@gmail.com>
2016-10-25 00:32:16 +0400

    Update main.c

--- lab3/ansi/main.c
+++ lab3/ansi/main.c
@@ -1,6 +1,8 @@
+/*директивы для подключения файлов */
 #include <stdio.h>
 #include <math.h>
-
+/* сообщает компилятору, что есть функция с именем main,
+ * и что функция возвращает целое число типа int */
 int main()
 {
     char r;
@@ -21,64 +23,64 @@ int main()
 
         printf("Введите шаг: \n");
         scanf("%lf", &step);
-            printf("Введите букву выражения, которое xотите вычислить - G,F,Y) \n");
-            scanf("%s", &r);
+        printf("Введите букву выражения, которое xотите вычислить - G,F,Y) \n");
+        scanf("%s", &r);
 
 
-            switch (r) {
-                case 'G':
-                    for (int i = 0; i < start_f; ++i) {
-                        y1 = -(8 * (12 * (a * a) + 68 * a * x + 63 * (x * x)));
-                        y2 = (4 * (a * a) + a * x - 5 * (x * x));
-                        if (y2 == 0.0) {
-                            printf("На ноль делить нельзя");
-                            return 0;
-                        }
-                        else {
-                            G = y1 / y2;
-                            printf("G = %lf \n", G);
-                            FILE *file;
-                            file = fopen("file_G.txt", "ab+");
-                            fprintf(file, "%lf \n", G);
-                            x = x + step;
-                        }
+        switch (r) {
+            case 'G':
+                for (int i = 0; i < start_f; ++i) {
+                    y1 = -(8 * (12 * (a * a) + 68 * a * x + 63 * (x * x)));
+                    y2 = (4 * (a * a) + a * x - 5 * (x * x));
+                    if (y2 == 0.0) {
+                        printf("На ноль делить нельзя");
+                        return 0;
                     }
-                    break;
-
-                case 'F':
-                    while (start_f < end_f) {
-                        double f_1, f_2;
-                        f_1 = (sin(pi * (40 * (a * a) - 61 * a * x + 7 * (x * x))));
-                        f_2 = (pi * (40 * (a * a) - 61 * a * x + 7 * (x * x)));
-                        if (f_2 == 0.0) {
-                            printf("На ноль делить нельзя");
-                            return 0;
-                        }
-                        else {
-                            F = f_1 / f_2;
-                            printf("F = %lf \n", F);
-                            FILE *file;
-                            file = fopen("file_F.txt", "ab+");
-                            fprintf(file, "%lf \n", F);
-                            x = x + step;
-                            start_f = start_f + 1;
-                        }
-                    }
-
-                    break;
-
-                case 'Y':
-                    while (start_f < end_f) {
-                        Y = -7 * (a * a) + 40 * a * x + 63 * (x * x) + 1;
-                        printf("Y = %lf \n", Y);
+                    else {
+                        G = y1 / y2;
+                        printf("G = %lf \n", G);
                         FILE *file;
-                        file = fopen("file_Y.txt", "ab+");
-                        fprintf(file, "%lf \n", Y);
+                        file = fopen("file_G.txt", "ab+");
+                        fprintf(file, "%lf \n", G);
+                        x = x + step;
+                    }
+                }
+                break;
+
+            case 'F':
+                while (start_f < end_f) {
+                    double f_1, f_2;
+                    f_1 = (sin(pi * (40 * (a * a) - 61 * a * x + 7 * (x * x))));
+                    f_2 = (pi * (40 * (a * a) - 61 * a * x + 7 * (x * x)));
+                    if (f_2 == 0.0) {
+                        printf("На ноль делить нельзя");
+                        return 0;
+                    }
+                    else {
+                        F = f_1 / f_2;
+                        printf("F = %lf \n", F);
+                        FILE *file;
+                        file = fopen("file_F.txt", "ab+");
+                        fprintf(file, "%lf \n", F);
                         x = x + step;
                         start_f = start_f + 1;
                     }
-                    break;
-            }
+                }
+
+                break;
+
+            case 'Y':
+                while (start_f < end_f) {
+                    Y = -7 * (a * a) + 40 * a * x + 63 * (x * x) + 1;
+                    printf("Y = %lf \n", Y);
+                    FILE *file;
+                    file = fopen("file_Y.txt", "ab+");
+                    fprintf(file, "%lf \n", Y);
+                    x = x + step;
+                    start_f = start_f + 1;
+                }
+                break;
+        }
 
         printf("Вычислим еще раз? (1-да,2-нет) \n");
         scanf("%lf", &run);
