ba146b920c3e33e138b6e602b11aaeb417d6a8bd
KiraLow <3oonekro@gmail.com>
2016-12-11 18:18:13 +0400

    подсчет четных чисел

--- lab5/ansi/main.c
+++ lab5/ansi/main.c
@@ -1,122 +1,188 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <memory.h>
+#include <string.h>
 
 int main() {
     char r;
-    double G, F, Y, g_1, g_2;
+    double G, F, Y, G1, G2;
     const double pi = 3.14159265358979;
-    double step_for_x, step_for_a;
-    double x1, x2, a1, a2, x_mod, a_mod;
-    char rec[100];
-    char change[255];
+    double StepForX, StepForA;
+    double x2, a2, xMod, aMod;
+    int kol_step = 0,j = 0, cnt = 0;
     double run = 1;
-    int kol_step;
-
+    char rec[100],result[255] = {0},temp[255],example[255] = {0};
     do {
 
-        printf("%s", "Введите первую границу для аргумента: \n");
+        printf("Введите первую границу для аргумента: \n");  /*Организовать ввод начального и конечного значений аргумента*/
         scanf("%s", rec);
-        x1 = atof(rec);
+        xMod = atof(rec);  /*использовать функции преобразования из строки в число*/
 
-        printf("%s", "Введите вторую границу для аргумента: \n");
+        printf("Введите вторую границу для аргумента: \n");
         scanf("%s", rec);
         x2 = atof(rec);
 
-        printf("%s", "Введите первую границу для параметра: \n");
+        printf("Введите первую границу для параметра: \n"); /*...и  параметра*/
         scanf("%s", rec);
-        a1 = atof(rec);
+        aMod = atof(rec);
 
-        printf("%s", "Введите вторую границу для параметра: \n");
+        printf("Введите вторую границу для параметра: \n");
         scanf("%s", rec);
         a2 = atof(rec);
 
-        printf("%s", "Введите количество шагов: \n");
+        printf("Введите количество шагов: \n");  /*количество шагов расчета функций G, F, Y*/
         scanf("%s", rec);
-        kol_step = (int) atof(rec);
+        kol_step = atoi(rec);
+        double mass[kol_step];
 
-        step_for_x = (x2 - x1) / kol_step;
-        step_for_a = (a2 - a1) / kol_step;
+        StepForX = (x2 - xMod) / (kol_step - 1);
+        StepForA = (a2 - aMod) / (kol_step - 1);
 
         printf("Введите букву выражения, которое xотите вычислить - G,F,Y) \n");
         scanf("%s", &r);
         int i = 0;
-        int j = 0;
-        x_mod = x1;
-        a_mod = a1;
-        double mass[3][kol_step];
+        int k = 0;
+
         switch (r) {
             case 'G':
                 printf("Значение функции ");
                 printf(" Значение X\n");
-                int m = 0;
-                for (m = 0; m < kol_step; ++m) {
-                    g_1 = -(8 * (12 * (a_mod * a_mod) + 68 * a_mod * x_mod + 63 * (x_mod * x_mod)));
-                    g_2 = (4 * (a_mod * a_mod) + a_mod * x_mod - 5 * (x_mod * x_mod));
-                    if (g_2 != 0) {
-                        G = g_1 / g_2;
-                        printf("%12.0lf ", G);
-                        printf("%10.0lf ", x_mod);
-                        printf("%10.0lf ", a_mod);
+                while (kol_step > j) {
+                    G1 = -(8 * (12 * (aMod * aMod) + 68 * aMod * xMod + 63 * (xMod * xMod)));
+                    G2 = (4 * (aMod * aMod) + aMod * xMod - 5 * (xMod * xMod));
+                    if (G2 <= 0.00000000001 && G2 >= -0.00000000001) {
+                        printf("На ноль делить нельзя");
+                    } else {
+                        G = G1 / G2;
+                        printf("%12.5lf ", G);
+                        printf("%13.5lf ", xMod);
                         printf("\n");
-                        mass[0][j] = G;
-                        mass[1][j] = x_mod;
-                        mass[2][j] = a_mod;
-                        x_mod += step_for_x;
-                        a_mod += step_for_a;
+                        mass[i] = G;  /*сохранением результатов в массив*/
+                        i += 1;
+                        xMod += StepForX;
+                        aMod += StepForA;
                         j+=1;
                     }
                 }
-                printf("значения \n");
-                int k = 0;
+
+                double minG = mass[0], maxG = mass[0];
+
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] > maxG) {
+                        maxG = mass[k];
+                    }
+                }
+                printf("Максимальное значение в массиве - %lf\n", maxG);
+
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] < minG) {
+                        minG = mass[k];
+                    }
+                }
+                printf("Минимальное значение в массиве - %lf\n", minG);
                 int l = 0;
-                char mass0[300], mass1[300];
-                for (k = 0; k < kol_step; k++) {
-                    sprintf(mass0, "%.2lf ", mass[0][k]);/* в общем тут в массив mass0  я кладу первую строчку (ибо 0 это первая строчка) массива mass */
-                    sprintf(mass1, "%.2lf ", mass[1][k]);/* тут вторую строчку в другой массив, mass 1*/
-                    strcat(mass0,mass1); /* бай бай*/
-                    printf("%s", mass0); /* выводим - профит!*/ /* ща попробую*/
+                for (l = 0; l < i; ++l) {
+                    sprintf(temp, "%lf", mass[l]);
+                    strcat(result,temp);
+                }
+                printf("*Полученный массив результатов представить в виде одной строки*\n%s\n",result);
+
+                int i=0,k=0,m=0,num2 = 0;
+                /*Поиск подстроки*/
+                scanf("%s", example);
+                l=0;
+                m=0;
+                while (m != 255 && example[m] !=0) {  /*подсчитывающую количество четных цифр в введенном числе*/
+                    if ((example[m] % 2) == 0) {
+                        m++;
+                        l++;
+                    } else {
+                        m++;
+                    }
+
                 }
 
-                    break;
+                printf("%i",l);
 
-                    case 'F':
-                        while (x_mod < x2) {
-                            double f_1, f_2;
-                            f_1 = (sin(pi * (40 * (a_mod * a_mod) - 61 * a_mod * x_mod + 7 * (x_mod * x_mod))));
-                            f_2 = (pi * (40 * (a_mod * a_mod) - 61 * a_mod * x_mod + 7 * (x_mod * x_mod)));
-                            if (f_2 != 0.0) {
-                                F = f_1 / f_2;
-                                printf("F = %lf", F);
-                                printf(" при x = %lf", x_mod );
-                                mass[0][j] = F;
-                                mass[1][j] = x_mod;
-                                mass[2][j] = a_mod;
-                                x_mod += step_for_x;
-                                a_mod += step_for_a;
-                            }
+                int n=0;
+                while (n<strlen(result)) {
+                    if (strstr(result,example) - result + 1 < strlen(result)) {
+                        i++;  /*В найденной последовательности найти строку совпадающую с шаблоном*/
+                        n = (int) (strstr(result, example) - result + 1);
+                        while (k!=n) {
+                            result[k] = '/';
+                            k++;
                         }
-                    break;
-
-                    case 'Y':
-                        while (x_mod < x2) {
-                            Y = -7 * (a_mod * a_mod) + 40 * a_mod * x_mod + 63 * (x_mod * x_mod) + 1;
-                            mass[0][j] = Y;
-                            mass[1][j] = x_mod;
-                            mass[2][j] = a_mod;
-                            printf("Y = %lf", mass[i][j]);
-                            x_mod += step_for_x;
-                            a_mod += step_for_a;
-                        }
-                    break;
-
-                    default:
-                        printf("Что то не то, введи еще раз");
+                        k=0;
+                    }
+                    if (strstr(result,example) == 0)
+                        break;
                 }
-                printf("Вычислим еще раз? (1-да,2-нет) \n");
-                scanf("%lf", &run);
-    }
-    while (run == 1);
+                printf("Найдено совпадений: %i\n",i); /*Вывести количество найденных совпадений в исходной строке*/
+
+                break;
+
+            case 'F':
+                while (xMod < x2) {
+                    double F1, F2;
+                    F1 = (sin(pi * (40 * (aMod * aMod) - 61 * aMod * xMod + 7 * (xMod * xMod))));
+                    F2 = (pi * (40 * (aMod * aMod) - 61 * aMod * xMod + 7 * (xMod * xMod)));
+                    if (F2 >= -0.00000000001 && F2 <= 0.000000001) {
+                        F = F1 / F2;
+                        printf("F = %lf", F);
+                        printf(" при x = %lf", xMod);
+                        mass[i] = F;
+                        i =+ 1;
+                        xMod += StepForX;
+                        aMod += StepForA;
+                    }
+                }
+                double minF = mass[0], maxF = mass[0];
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] > maxF) {
+                        maxF = mass[k];
+                    }
+                }
+                printf("Максимальное значение в массиве - %lf\n", maxF);
+
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] < minF) {
+                        minF = mass[k];
+                    }
+                }
+                printf("Минимальное значение в массиве - %lf\n", minF);
+                break;
+
+            case 'Y':
+                while (xMod < x2) {
+                    Y = -7 * (aMod * aMod) + 40 * aMod * xMod + 63 * (xMod * xMod) + 1;
+                    printf("Y = %lf", Y);
+                    printf(" при x = %lf", xMod);
+                    mass[i] = Y;
+                    i += 1;
+                    xMod += StepForX;
+                    aMod += StepForA;
+                }
+                double minY = mass[0], maxY = mass[0];
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] > maxY) {
+                        maxY = mass[k];
+                    }
+                }
+                printf("Максимальное значение в массиве - %lf\n", maxY);
+
+                for (k = 0; k < i; ++k) {
+                    if (mass[k] < minY) {
+                        minY = mass[k];
+                    }
+                }
+                printf("Минимальное значение в массиве - %lf\n", minY);
+                break;
+            default:
+                printf("Введенное значение не обнаружено, пожалуйста, введите букву выражения, которое хотите вычислить - F,G,Y\n");
+        }
+        printf("Вычислим еще раз? (1-да,2-нет) \n");
+        scanf("%lf", &run);
+    } while (run == 1);
     return 0;
 }
