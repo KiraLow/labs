7b011cec92e6a9b5a710c906f9f86dca4ec357c6
KiraLow <3oonekro@gmail.com>
2016-12-12 21:26:21 +0400

    Create koren.py

---
+++ semesrovye/koren/python/koren.py
@@ -0,0 +1,11 @@
+x = float(input("Число, у которого ищем квадратный корень"))
+e = float(input("Введем точность, с которой хотим вычислить"))
+yi = float(x)
+y = float()
+y = yi
+yi = (y + x / y) / 2
+if (y - yi) >= e:
+    while (y - yi) >= e:
+        y = yi
+        yi = (y + x / y) / 2
+print("Результат = ", y)
