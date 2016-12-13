6d9f60778d8974ff03dc7d0f69ed0a380d2e4654
KiraLow <3oonekro@gmail.com>
2016-12-13 20:09:14 +0400

    Update main.c

--- lab8/ansi/main.c
+++ lab8/ansi/main.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int myRandForX(int axisX, int axisXend);
+int myRandForX(int axisX, int axisEnd);
 int myRandForY(int axisY, int axisYend);
 
 int main() {
     double rad;
-    int axisStartForX, axisXend, axisYend, axisStartForY;
+    int axisStartForX, axisEnd, axisStartForY;
     double valuesX, valuesY;
-    int i = 0,l = 0,kol_dot;
-    int x[250];
-    int y[250];
+    int i = 0,l = 0, kol_dot;
 
     printf("Введите параметры поиска\n\nНачальное значение оси: ");
     scanf("%i", &axisStartForX);
     axisStartForY = axisStartForX;
 
-    printf("Конечное значение по оси X: ");
-    scanf("%i", &axisXend);
-
-    printf("Конечное значение по оси Y: ");
-    scanf("%i", &axisYend);
-
-    kol_dot = 1 + rand() % (249);
-    for (i = 0; i < kol_dot; ++i) {
-        x[i] = myRandForX(axisStartForX, axisXend);
-        y[i] = myRandForY(axisStartForY, axisYend);
-    }
-
-    i = 1 + rand() % (249); /*тот же алгоритм, (250-1) */
-    printf("Координаты случайно выбранной точки [%i,%i]\n", x[i],y[i]);
-
+    printf("Конечное значение по оси");
+    scanf("%i", &axisEnd);
 
     printf("Введите радиус окружности, в которой будем искать точки ");
     scanf("%lf", &rad);
+    FILE *file;
+    file = fopen("//home//iarven//prog//time.txt", "w+");
 
-    i=0;
-    while (x[i] != 0) {
-        if (x[i] * x[i] + y[i] * y[i] <= rad * rad) {
-            l++; /*+1 к счетчику, принадлежит*/
+    for (kol_dot = 10; kol_dot < 5000; kol_dot+=100) { /*каждую итерацию увеличиваем
+ * количество точек*/
+
+        clock_t time; /* найдем время выполнения программы */
+        time = clock();
+
+        int x[kol_dot];
+        int y[kol_dot];
+
+        for (i = 0; i < kol_dot; ++i) {
+            x[i] = myRandForX(axisStartForX, axisEnd);
+            y[i] = myRandForY(axisStartForY, axisEnd);
         }
-        i++;
+
+        i = 1 + rand() % (kol_dot - 1); /*тот же алгоритм, (250-1) */
+        printf("Координаты случайно выбранной точки [%i,%i]\n", x[i], y[i]);
+
+        i = 0;
+        while (x[i] != 0) {
+            if (x[i] * x[i] + y[i] * y[i] <= rad * rad) {
+                l++; /*+1 к счетчику, принадлежит*/
+            }
+            i++;
+        }
+        printf("Количество точек, принадлежащих окружности составляет: %i\n", l);
+
+        time = clock() - time;
+        printf("%f\n", (double)time/CLOCKS_PER_SEC);
+        fprintf(file, "Количество точек = %i", l);
+        fprintf(file, "\t\tВремя%f\n", (double)time/CLOCKS_PER_SEC);
     }
-    printf("Количество точек, принадлежащих окружности составляет: %i\n",l);
+    fclose(file);
 }
 
 int myRandForX(int x, int xEnd) {
