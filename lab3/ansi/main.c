69471f8459fbd57372cd2e3ce2bcc2d0fc38e318
KiraLow <3oonekro@gmail.com>
2016-12-20 12:42:18 +0400

    Add files via upload

--- lab3/ansi/main.c
+++ lab3/ansi/main.c
@@ -41,12 +41,12 @@ int main()
                             }
                             else {
                                 G = g_1 / g_2;
-                                printf("G = %.10lf ", G);
-                                printf("step = %5.0lf ", step_for_x);
+                                printf("G = %.5lf ", G);
+                                printf("step = %5.10lf ", step_for_x);
                                 x_mod += step_for_x;
                                 diff_G = -(8 * (12 * (a * a) + 68 * a * x_mod + 63 * (x_mod * x_mod))) /
                                            (4 * (a * a) + a * x_mod - 5 * (x_mod * x_mod));
-                                printf("diffG = %5.10lf \n ", G);
+                                printf("diffG = %5.5lf \n ", diff_G);
                                 if (diff < fabs(diff_G - G)) {
                                     step_for_x /=2;
                                     x_mod +=step_for_x;
@@ -63,11 +63,11 @@ int main()
                         f_2 = (pi * (40 * (a * a) - 61 * a * x_mod + 7 * (x_mod * x_mod)));
                         if (f_2 != 0.0 ) {
                             F = f_1/f_2;
-                            printf("F = %.20lf", F);
-                            printf("step = %5.15lf", step_for_x);
+                            printf("F = %.15lf", F);
+                            printf("\tstep = %5.7lf", step_for_x);
                             x_mod += step_for_x;
                             diff_F = ((sin(pi * (40 * (a * a) - 61 * a * x_mod + 7 * (x_mod * x_mod)))) /(pi * (40 * (a * a) - 61 * a * x_mod + 7 * (x_mod * x_mod))));
-                            printf("diffF = %.20lf \n", diff_F);
+                            printf("\tdiffF = %.15lf \n", diff_F);
                             if (diff < fabs(diff_F - F)) {
                                 step_for_x /=2;
                                 x_mod += step_for_x;
@@ -81,32 +81,23 @@ int main()
                     while (x_mod <= x2) {
                         Y = -7 * (a * a) + 40 * a * x_mod + 63 * (x_mod * x_mod) + 1;
                         printf("Y = %lf", Y);
-                        printf("step = %5.15lf", step_for_x);
+                        printf("\tstep = %5.15lf", step_for_x);
                         x_mod += step_for_x;
                         diff_Y = -7 * (a * a) + 40 * a * x_mod + 63 * (x_mod * x_mod) + 1;
-                        printf("diffY = %.20lf \n", diff_Y);
-                        if (diff < fabs(diff_Y) - abs(Y)) {
+                        printf("\tdiffY = %lf \n", diff_Y);
+                        if (diff < fabs(diff_Y - Y)) {
                             step_for_x /= 2;
-                            x_mod+=step_for_x;
-                            j-=1;
-                        }
-                        if (1 > fabs(diff_Y) - abs(Y)) {
-                            step_for_x *= 2;
                             x_mod+=step_for_x;
                             j-=1;
                         }
                         j+=1;
                     }
-
                 default:
                     printf("Что-то не то, давай еще раз \n");
             }
-
         printf("Вычислим еще раз? (1-да,2-нет) \n");
         scanf("%i", &run);
-
     }
     while (run==1);
-
     return 0;
 }
