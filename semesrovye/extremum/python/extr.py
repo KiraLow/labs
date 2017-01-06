dba0979a51658082139034aa44430fb411bf4992
kiralow <dimaisin@yandex.ru>
2017-01-06 13:27:36 +0200

    for U

--- semesrovye/extremum/python/extr.py
+++ semesrovye/extremum/python/extr.py
@@ -1,27 +1,15 @@
 def function(x1):
-    return x1 ** 2 + 5 * x1 + 10
-
-def kol(xEnd,xStart):
-    while xEnd - xStart < e:
-        x = (xEnd + xStart) / 2
-        f1 = function(x) - e
-        f2 = function(x) + e
-        if f1 < f2:
-            xEnd = x
-        else:
-            xStart = x
-    return xStart
+    return x1*x1*4+x1*2
 
 xStart = float(input())
 xEnd = float(input())
 e = float(input())
-
-x = (xEnd + xStart) / 2
-f1 = function(x - e)
-f2 = function(x + e)
-if f1 < f2:
-    xEnd = x
-else:
-    xStart = x
-
-print(kol(xEnd,xStart))
+while abs(xStart - xEnd) > e:
+    x = (xEnd + xStart) / 2
+    f1 = function(x - e)
+    f2 = function(x + e)
+    if f1 < f2:
+        xEnd = x
+    else:
+        xStart = x
+print(xEnd)
