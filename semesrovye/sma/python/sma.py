43d34fe98b8188cd840911f12a3517d11a95c979
kiralow <dimaisin@yandex.ru>
2017-01-08 21:57:36 +0200

    sma python

--- semesrovye/sma/python/sma.py
+++ semesrovye/sma/python/sma.py
@@ -1,13 +1,15 @@
 import random
 import pylab
-mas = [10]
-for i in range(9):
-    mas.append(random.randrange(1,9))
+countRow = int(input())
+mas = [countRow]
+for i in range(countRow - 1):
+    mas.append(random.randrange(1,9)*2 / 3.2)
 print("\n", mas)
 
-dot = [10]
-y = [1,2,3,4,5,6,7,8,9,10]
-for l in range(9):
+dot = [countRow]
+y = [i for i in range(countRow)]
+l = 1
+for l in range(countRow-1):
     dot.append((mas[l-1] + mas[l] + mas[l+1]) / 3)
 print(dot)
 pylab.plot(y, mas)
