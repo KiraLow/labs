9f7180661b9f276bd1a307d33f61af1ae6a453c7
kiralow <dimaisin@yandex.ru>
2017-01-08 16:07:55 +0200

    fkng blur in ansi

---
+++ semesrovye/img/imgGauss/ansi/cmake-build-debug/CMakeFiles/imageload_template.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageload_template.dir/main.c.o"
+  "CMakeFiles/imageload_template.dir/qdbmp.c.o"
+  "imageload_template.pdb"
+  "imageload_template"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/imageload_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ semesrovye/img/sobel/ansi/cmake-build-debug/CMakeFiles/imageload_template.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vlad/Prg/semesrovye/img/sobel/ansi/main.c" "/home/vlad/Prg/semesrovye/img/sobel/ansi/cmake-build-debug/CMakeFiles/imageload_template.dir/main.c.o"
+  "/home/vlad/Prg/semesrovye/img/sobel/ansi/qdbmp.c" "/home/vlad/Prg/semesrovye/img/sobel/ansi/cmake-build-debug/CMakeFiles/imageload_template.dir/qdbmp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
