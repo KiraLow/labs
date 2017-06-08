42480bb96fbff002dde53b8fda53ecffe48ea8b5
KiraLow <3oonekro@gmail.com>
2017-06-08 14:56:23 +0300

    Update main.c

--- sem_2/lab_1/1_6/main.c
+++ sem_2/lab_1/1_6/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <memory.h>
-
 int main() {
     char symb = 'q';
-    char *tmp = NULL, *b = NULL;
+    char *tmp, *b;
     int i = 1;
-
     tmp = malloc(2 * sizeof(char));
     b = malloc(sizeof(char));
-
-    while (symb != 'w') {
-        scanf("%s", &symb);
-        getchar();
+    *tmp = NULL;
+    *b = NULL;
+    while (symb != ' ') {
+        gets(&symb);
         if ((symb == 'e') || (symb == 'E') || (symb == 'u') || (symb == 'U') || (symb == 'o') || (symb == 'O') ||
             (symb == 'a') || (symb == 'A') || (symb == 'i') || (symb == 'I')) {
             sprintf(b, "%s", &symb);
@@ -20,6 +19,6 @@ int main() {
             tmp = realloc(tmp, 1 + sizeof(char));
         }
     }
+    printf("%s", tmp);
     return 0;
-
 }
